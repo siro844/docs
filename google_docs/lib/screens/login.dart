92aca85a84907f125cf4536bd4e7825d513b5ad1
siro844 <140879194+siro844@users.noreply.github.com>
2023-12-06 00:19:27 +0530

    misc

--- google_docs/lib/screens/login.dart
+++ google_docs/lib/screens/login.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_docs/colors.dart';
+import 'package:google_docs/repository/auth_repository.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends ConsumerWidget {
   const LoginScreen({super.key});
+  void signInwithGoogle(WidgetRef ref){
+    ref.read(authRepositoryProvider).signInWithGoogle();
+  }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context,WidgetRef ref) {
+    ref.watch(authRepositoryProvider).signInWithGoogle();
     return Scaffold(
      body: Center(
       child: ElevatedButton.icon(
@@ -13,7 +19,9 @@ class LoginScreen extends StatelessWidget {
           minimumSize: const Size(150, 50),
           backgroundColor: white,
         ),
-        onPressed: (){},
+        onPressed: (){
+          signInwithGoogle(ref);
+        },
        icon: Image.asset("assets/google_logo.png",height: 30,width: 30,), 
        label:const Text("Sign in with Google",style: TextStyle(color: black),), ),
      ),
