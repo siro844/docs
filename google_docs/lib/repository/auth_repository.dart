8187fa25e904c1d3e8b701c87a3652aba9990c77
siro844 <140879194+siro844@users.noreply.github.com>
2023-12-06 00:14:06 +0530

    misc

--- google_docs/lib/repository/auth_repository.dart
+++ google_docs/lib/repository/auth_repository.dart
@@ -1,9 +1,30 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+
+final  authRepositoryProvider=Provider((ref) =>
+ AuthRepository(googleSignIn: GoogleSignIn())
+ );
 
 class AuthRepository {
   final GoogleSignIn _googleSignIn;
   AuthRepository({
     required GoogleSignIn googleSignIn,
   }):_googleSignIn=googleSignIn;
+
+
+  void signInWithGoogle() async {
+    try{
+     final user = await  _googleSignIn.signIn();
+      if(user!=null){
+        print(user.email);
+        print(user.photoUrl);
+        print(user.displayName);
+      }
+    }
+    catch(e){
+      print(e);
+    }
+  }
 }
+
