bdda64dd347d9f0b066ee71042334c27130c6b32
siro844 <140879194+siro844@users.noreply.github.com>
2023-12-05 23:54:24 +0530

    misc

--- google_docs/lib/screens/login.dart
+++ google_docs/lib/screens/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_docs/colors.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -8,9 +9,13 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
      body: Center(
       child: ElevatedButton.icon(
+        style: ElevatedButton.styleFrom(
+          minimumSize: const Size(150, 50),
+          backgroundColor: white,
+        ),
         onPressed: (){},
-       icon: Image.asset("assets/google_logo.png",height: 40,width: 40,), 
-       label:Text("Sign in with Google"), ),
+       icon: Image.asset("assets/google_logo.png",height: 30,width: 30,), 
+       label:const Text("Sign in with Google",style: TextStyle(color: black),), ),
      ),
     );
   }
