05284344be75c60517d4c13f6dc85e1428a99d3b
siro844 <140879194+siro844@users.noreply.github.com>
2023-12-06 20:07:47 +0530

    user

--- google_docs/server/routes/auth.js
+++ google_docs/server/routes/auth.js
@@ -1,11 +1,25 @@
 const express=require('express');
+const User = require('../models/user');
 // const app=express();
 const authRouter= express.Router();
 authRouter.post('/api/signup',async (req,res)=>{
     try{
         const {name,email,profilePic} =req.body;
-        
+        //if email doesnt exist only then store the data
+        //does the email exist??
+        let user = await User.findOne({
+            email:email
+        });
+        if(!user){
+            user = new User({
+                name:name,
+                email:email,
+                profilePic:profilePic,
+            });
+            user = await user.save();
+        }
 
+        res.json(user);
     }catch(e){
 
     }
